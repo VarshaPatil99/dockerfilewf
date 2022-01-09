47c48a86ae1ef8b49b2f0c4f648255ceabd504e3
Ketan J <ketanvj@yahoo.com>
2022-01-09 07:59:01 +0000

    modified command.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+docker build -t ketanvj/wf:v1.0 .
 export showlayers='{{ range .RootFS.Layers }}{{ println . }}{{end}}'
 docker inspect -f "$showlayers" ketanvj/wf:v1.0
+export showSize='{{ .Size }}'
+docker inspect -f "$showsize" ketanvj/wf:v1.0
