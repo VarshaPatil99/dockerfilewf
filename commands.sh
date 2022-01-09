c90f674cef3b4cde07bcb1e56710b024cd855894
Ketan J <ketanvj@yahoo.com>
2022-01-09 08:40:17 +0000

    final solution

--- commands.sh
+++ commands.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-docker build -t ketanvj/wf:v1.0 .
+docker build -t ketanvj/wf:v2.0 .
 export showlayers='{{ range .RootFS.Layers }}{{ println . }}{{end}}'
-docker inspect -f "$showlayers" ketanvj/wf:v1.0
+docker inspect -f "$showlayers" ketanvj/wf:v2.0
 export showSize='{{ .Size }}'
-docker inspect -f "$showsize" ketanvj/wf:v1.0
+docker inspect -f "$showsize" ketanvj/wf:v2.0
+docker run -itd --rm --name myapp1 -p 80:80 ketanvj/wf:v2.0
+# Now go to browser type your EC2 instance IP and see if you can get the website.
+
